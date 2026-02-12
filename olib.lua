abca93916bc5f5cfd450a469fa0f95027b05cd4c
wanhandredmail-art <wanhandredmail@gmail.com>
2026-02-12 20:11:15 +0300

    Update olib.lua

--- olib.lua
+++ olib.lua
@@ -30,20 +30,8 @@ local OrionLib = {
 --Feather Icons https://github.com/evoincorp/lucideblox/tree/master/src/modules/util - Created by 7kayoh
 local Icons = {}
 
-local Success, Response = pcall(function()
-	Icons = HttpService:JSONDecode(game:HttpGetAsync("https://raw.githubusercontent.com/evoincorp/lucideblox/master/src/modules/util/icons.json")).icons
-end)
-
-if not Success then
-	warn("\nOrion Library - Failed to load Feather Icons. Error code: " .. Response .. "\n")
-end	
-
 local function GetIcon(IconName)
-	if Icons[IconName] ~= nil then
-		return Icons[IconName]
-	else
-		return nil
-	end
+	return "rbxassetid://4483345998"
 end   
 
 local Orion = Instance.new("ScreenGui")
